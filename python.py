e7ebccc7c5c65b1af59b4247ea4d5b04055ced73
Quy <nguyenphuquy2k5@gmail.com>
2025-08-28 20:03:38 +0700

    in

--- python.py
+++ python.py
@@ -1,1 +1,16 @@
 print("Hello World! ")
+if 5 > 2:
+    print("Five is greater than two!")
+    
+total = 1 + \
+2 + \
+3
+print(total);print("Hello Python")
+
+if True:
+    print("True")
+else:
+    print("False")
+
+# This is a comment
+print("Hello, World!")
