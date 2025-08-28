b353d69eaa2aa8ad7b235f768158943adb1e92ba
Quy <nguyenphuquy2k5@gmail.com>
2025-08-28 20:41:20 +0700

    the

--- python1.py
+++ python1.py
@@ -1,2 +1,9 @@
 print("Albert Einstein đã từng nói: Một người không bao giờ mắc sai lầm không bao giờ thử bất kỳ điều gì mới")
 print("Chao cac ban")
+
+import module1
+import module2
+print(module1.question)
+print(module2.question)
+print(module1.answer)
+print(module2.answer)
